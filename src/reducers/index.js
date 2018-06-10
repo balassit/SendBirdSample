73e4669b70c096a6fe41faab811d41383dce6426
Lassiter, Blake <Blake.Lassiter@capitalone.com>
2018-06-09 19:05:42 -0700

    Login functionality (#3)
    * move files to src, add dependencies for auth
    
    * sign in page appears
    
    * update config
    
    * successful login
    
    * successful account creation, login, logout, signup logic

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,6 @@
-import { combineReducers } from 'redux';
- 
-import { DATA_AVAILABLE } from "../actions/" //Import the actions types constant we defined in our actions
- 
-let dataState = { data: [], loading:true };
- 
-const dataReducer = (state = dataState, action) => {
-    switch (action.type) {
-        case DATA_AVAILABLE:
-            state = Object.assign({}, state, { data: action.data, loading:false });
-            return state;
-        default:
-            return state;
-    }
-};
- 
-// Combine all the reducers
-const rootReducer = combineReducers({
-    dataReducer
-    // ,[ANOTHER REDUCER], [ANOTHER REDUCER] ....
+import { combineReducers } from 'redux'
+import auth from './auth'
+
+export default combineReducers({
+  auth
 })
- 
-export default rootReducer;
