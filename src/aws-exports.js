b37289de8bfe5d21cd30e47153155858c7ba548c
Lassiter, Blake <Blake.Lassiter@capitalone.com>
2018-06-09 17:46:26 -0700

    Authenticate User Sign in (#1)
    * move files to src, add dependencies for auth
    
    * sign in page appears
    
    * update config
    
    * successful login

---
+++ src/aws-exports.js
@@ -0,0 +1,7 @@
+const config = {
+    identityPoolId: 'us-west-2:00c0984a-79c8-4cb0-b459-0cecc06ea879',
+    region: 'us-west-2',
+    userPoolId: 'us-west-2_ETTqmaJWc',
+    userPoolWebClientId: '6agn9poglg2o4seobjtad7fglp'
+  }
+export default config
